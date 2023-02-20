7ec37eee36d11805f620227ecbe6fb644197a120
Yijia Zhao <zhao.yijia@ufl.edu>
2023-02-20 16:27:56 -0500

    directional sprite for moving & resting

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,18 +9,67 @@ public class Player : MonoBehaviour
     float horizontal;
     float vertical;
     float moveLimiter = 0.7f;
-
     public float runSpeed = 20.0f;
+    public SpriteRenderer spriteRenderer;
+    public Sprite restingUpSprite;
+    public Sprite restingDownSprite;
+    public Sprite restingLeftSprite;
+    public Sprite restingRightSprite;
+    public Sprite movingUpSprite;
+    public Sprite movingDownSprite;
+    public Sprite movingLeftSprite;
+    public Sprite movingRightSprite;
 
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal"); // -1 is left
         vertical = Input.GetAxisRaw("Vertical"); // -1 is down
+
+        // Change sprite based on movement state and direction
+        if (horizontal != 0 || vertical != 0)
+        {
+            if (horizontal > 0)
+            {
+                spriteRenderer.sprite = movingRightSprite;
+            }
+            else if (horizontal < 0)
+            {
+                spriteRenderer.sprite = movingLeftSprite;
+            }
+            else if (vertical > 0)
+            {
+                spriteRenderer.sprite = movingUpSprite;
+            }
+            else if (vertical < 0)
+            {
+                spriteRenderer.sprite = movingDownSprite;
+            }
+        }
+        else
+        {
+            if (horizontal > 0)
+            {
+                spriteRenderer.sprite = restingRightSprite;
+            }
+            else if (horizontal < 0)
+            {
+                spriteRenderer.sprite = restingLeftSprite;
+            }
+            else if (vertical > 0)
+            {
+                spriteRenderer.sprite = restingUpSprite;
+            }
+            else if (vertical < 0)
+            {
+                spriteRenderer.sprite = restingDownSprite;
+            }
+        }
     }
 
     void FixedUpdate()
