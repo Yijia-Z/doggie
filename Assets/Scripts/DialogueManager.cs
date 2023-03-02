d55625f1e88bccc8558c484d2ba11639ffc24795
scarpio02 <scarpio@ufl.edu>
2023-03-01 21:00:20 -0500

    dialogue scripts

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Image actorImage;
+    public TextMeshProUGUI actorName;
+    public TextMeshProUGUI messageText;
+    public RectTransform backgroundBox;
+
+    Message[] currentMessages;
+    Actor[] currentActors;
+    int activeMessage = 0;
+    public static bool isActive = false;
+
+    public void OpenDialogue(Message[] messages, Actor[] actors)
+    {
+        currentMessages = messages;
+        currentActors = actors;
+        activeMessage = 0;
+        isActive = true;
+        backgroundBox.localScale = Vector3.one;
+        Debug.Log("Started conversation! Loaded messages: " + messages.Length);
+        DisplayMessage();
+    }
+
+    void DisplayMessage()
+    {
+        Message messageToDisplay = currentMessages[activeMessage];
+        messageText.text = messageToDisplay.message;
+
+        Actor actorToDisplay = currentActors[messageToDisplay.actorId];
+        actorName.text = actorToDisplay.name;
+        actorImage.sprite = actorToDisplay.sprite;
+    }
+
+    public void NextMessage()
+    {
+        activeMessage++;
+        if (activeMessage < currentMessages.Length)
+        {
+            DisplayMessage();
+        }
+        else
+        {
+            Debug.Log("Conversation ended!");
+            isActive = false;
+            backgroundBox.localScale = Vector3.zero;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        backgroundBox.localScale = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isActive)
+        {
+            NextMessage();
+        }
+    }
+}
