5c57e86b0b45a85f84679272e02f096917e37f3b
Yijia Zhao <zhao.yijia@ufl.edu>
2023-03-26 23:29:55 -0400

    fix dating progress

--- Assets/Scripts/DatingProgress.cs
+++ Assets/Scripts/DatingProgress.cs
@@ -1,16 +1,24 @@
-public static class DatingProgress {
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatingProgress : MonoBehaviour
+{
     private const int NUM_OWNERS = 3;
     private const int NUM_STAGES = 3;
 
     // Save the dating progress for the specified owner and stage.
-    public static void SaveProgress(int ownerIndex, int stageIndex) {
+    public static void SaveProgress(int ownerIndex, int stageIndex)
+    {
         PlayerPrefs.SetInt("OwnerIndex", ownerIndex);
         PlayerPrefs.SetInt("StageIndex", stageIndex);
     }
 
     // Load the saved dating progress.
-    public static bool LoadProgress(out int ownerIndex, out int stageIndex) {
-        if (PlayerPrefs.HasKey("OwnerIndex") && PlayerPrefs.HasKey("StageIndex")) {
+    public static bool LoadProgress(out int ownerIndex, out int stageIndex)
+    {
+        if (PlayerPrefs.HasKey("OwnerIndex") && PlayerPrefs.HasKey("StageIndex"))
+        {
             ownerIndex = PlayerPrefs.GetInt("OwnerIndex");
             stageIndex = PlayerPrefs.GetInt("StageIndex");
             return true;
@@ -21,22 +29,26 @@ public static class DatingProgress {
     }
 
     // Set the availability of the specified owner.
-    public static void SetOwnerAvailability(int ownerIndex, bool isAvailable) {
+    public static void SetOwnerAvailability(int ownerIndex, bool isAvailable)
+    {
         PlayerPrefs.SetInt("Owner" + ownerIndex + "Availability", isAvailable ? 1 : 0);
     }
 
     // Check if the specified owner is available.
-    public static bool IsOwnerAvailable(int ownerIndex) {
+    public static bool IsOwnerAvailable(int ownerIndex)
+    {
         return PlayerPrefs.GetInt("Owner" + ownerIndex + "Availability", 1) == 1;
     }
 
     // Mark the specified owner as unavailable.
-    public static void MarkOwnerAsUnavailable(int ownerIndex) {
+    public static void MarkOwnerAsUnavailable(int ownerIndex)
+    {
         SetOwnerAvailability(ownerIndex, false);
     }
 
     // Get the dialogue text for the specified owner and stage.
-    public static string GetDialogueText(int ownerIndex, int stageIndex) {
+    public static string GetDialogueText(int ownerIndex, int stageIndex)
+    {
         return Resources.Load<TextAsset>("Dating/Owner" + ownerIndex + "/Stage" + stageIndex).text;
     }
 }
