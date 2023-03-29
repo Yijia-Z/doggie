14a5bb496b53b442ed7c252b6caba7f82b534bd9
scarpio02 <scarpio@ufl.edu>
2023-03-29 02:33:22 -0400

    script stuff

--- Assets/Scripts/Dialogue Scripts/DialogueManager.cs
+++ Assets/Scripts/Dialogue Scripts/DialogueManager.cs
@@ -25,6 +25,7 @@ public class DialogueManager : MonoBehaviour
     bool sceneSwitch = false;
     int sceneID;
     private bool isReacting = false;
+    public GameObject BlackPanel;
 
     // Called by Start Dialogue Trigger
     public void OpenDialogue(Message[] messages, Actor[] actors, Response[] responses, bool switchScene)
@@ -149,7 +150,37 @@ public class DialogueManager : MonoBehaviour
 
     public void SwitchScene()
     {
+        StartCoroutine(FadeBlackOut());
         SceneManager.LoadScene(sceneID);
+    }
+
+    public IEnumerator FadeBlackOut(bool fadeToBlack = true, int fadeSpeed = 5)
+    {
+        Color objectColor = BlackPanel.GetComponent<Image>().color;
+        float fadeAmount;
+
+        if (fadeToBlack)
+        {
+            while (BlackPanel.GetComponent<Image>().color.a < 1)
+            {
+                fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
+
+                objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+                BlackPanel.GetComponent<Image>().color = objectColor;
+                yield return null;
+            }
+        }
+        else
+        {
+            while (BlackPanel.GetComponent<Image>().color.a > 0)
+            {
+                fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
+
+                objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+                BlackPanel.GetComponent<Image>().color = objectColor;
+                yield return null;
+            }
+        }
     }
 
 
