701c91ccf50e6233d5ebc0fa23113e5102988086
Natalie Sesodia <70526708+nsesodia@users.noreply.github.com>
2023-04-11 16:43:08 -0400

    Merge branch 'master' of https://github.com/Yijia-Z/doggie

--- Assets/Scripts/Dog.cs
+++ Assets/Scripts/Dog.cs
@@ -7,7 +7,6 @@ public class Dog : MonoBehaviour
 {
     [SerializeField] Sprite dogImage;
     [SerializeField] string dogName = "default_name";
-    [SerializeField] string favoriteItem = "default_favorite";
     [SerializeField] int dogOwnerIndex;
 
     public GameObject attention;
@@ -55,8 +54,7 @@ public class Dog : MonoBehaviour
                 button.onClick.AddListener(() =>
                 {
                     Debug.Log(button.tag);
-                    
-                    item = CreateInstanceFromTag(button.tag,button.name);
+                    item = CreateInstanceFromTag(button.tag);
                 });
                 button.onClick.AddListener(OnClick);
             }
@@ -132,10 +130,7 @@ public class Dog : MonoBehaviour
         }
         if (totalHappiness > 100f)
         {
-            if (equippedApparel == null)
-            {
-                totalHappiness = 100f;
-            }
+            totalHappiness = 100f;
         }
     }
 
@@ -205,23 +200,20 @@ public class Dog : MonoBehaviour
         inventory_panel.SetActive(false);
     }
 
-    public static Item CreateInstanceFromTag(string tag, string itemName)
+    public static Item CreateInstanceFromTag(string tag)
     {
         Item item = null;
-        int rate = 1;
-        if (itemName==favoriteItem){
-            rate=2;
-        }
+
         switch (tag)
         {
             case "Food":
-                item = new Food(100, 30*rate, tag, null, "Delicious food for dogs");
+                item = new Food(100, 30, tag, null, "Delicious food for dogs");
                 break;
             case "Toy":
-                item = new Toy(100, 30*rate, tag, null, "A toy for dogs to catch");
+                item = new Toy(100, 30, tag, null, "A toy for dogs to catch");
                 break;
             case "Apparel":
-                item = new Apparel(100, 30*rate, tag, null, "Cloth for dogs to wear");
+                item = new Apparel(100, 30, tag, null, "Cloth for dogs to wear");
                 break;
             default:
                 Debug.Log("Invalid item tag: " + tag);
