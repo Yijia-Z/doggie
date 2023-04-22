cfaec65fb3382cc14e04ccf5892f85f809c355f2
scarpio02 <scarpio@ufl.edu>
2023-04-22 14:20:50 -0400

    owner dialogue trigger

--- Assets/Scripts/Npc.cs
+++ Assets/Scripts/Npc.cs
@@ -7,12 +7,17 @@ using UnityEngine.SceneManagement;
 public class Npc : MonoBehaviour
 {
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log("Collission detected...");
         if (collision.gameObject.CompareTag("Player") == true)
         {
+            Debug.Log("..collission with player...");
             if (gameObject.GetComponent<SpriteRenderer>().color.a == 1)
+            {
+                Debug.Log("...fading black");
                 StartCoroutine(FindObjectOfType<Clock>().fadeBlack());
+            }
         }
     }
 }
